1a56dd88f4903b711d1e98b4532db8fbeec400f5
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-06-12 22:35:50 -0300

    plugin para desenvolver a localidade da palestra e começando a desenvolver o shortcode

---
+++ al_local_dia_palestra.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    * Plugin Name: Cadastrar local e data da palestra
+    * Description: Plugin para cadastrar local e data da proxima palestra realizada pela Alura
+    * Version: 1.0.0
+    * Author: Vítor Cavalcante Souza
+*/
+
+if (!defined('ABSPATH')) {
+    die;
+}
+
+
+require_once plugin_dir_path(__FILE__) . '/includes/al_local_dia_palestra_settings.php';
